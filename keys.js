dc66c325a0a70e020a3964104db75f86988f090d
pootispow1182 <ashtonharrington334@gmail.com>
2026-09-17 12:58:50 -0700

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,56 @@
+var KEY = { UP:87, LEFT:65, DOWN:83, RIGHT:68, SPACE:32 };
+var keyLeft = false;
+var keyRight = false;
+var keyUp = false;
+var keyDown = false;
+var keySpace = false;
+
+function keyD(event) {
+	var code = event.keyCode;
+	switch(code) {
+		case KEY.UP:
+		keyUp = true;
+		break;
+
+		case KEY.LEFT:
+		keyLeft = true;
+		break;
+
+		case KEY.RIGHT:
+		keyRight = true;
+		break;
+
+		case KEY.DOWN:
+		keyDown = true;
+		break;
+
+		case KEY.SPACE:
+		keySpace = true;
+		break;
+	}
+}
+
+function keyU(event) {
+	var code = event.keyCode;
+	switch(code) {
+		case KEY.UP:
+		keyUp = false;
+		break;
+
+		case KEY.LEFT:
+		keyLeft = false;
+		break;
+
+		case KEY.RIGHT:
+		keyRight = false;
+		break;
+
+		case KEY.DOWN:
+		keyDown = false;
+		break;
+
+		case KEY.SPACE:
+		keySpace = false;
+		break;
+	}
+}
